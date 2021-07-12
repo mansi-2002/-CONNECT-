b81d01821dbd75553c093a7eb6ba2b0cab4b8811
mansi-2002 <83465248+mansi-2002@users.noreply.github.com>
2021-07-12 12:19:17 +0530

    Add files via upload

---
+++ connect-frontend/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import { useEffect } from 'react';
+import { connectWithWebSocket } from './utils/wssConnection/wssConnection';
+import Dashboard from './Dashboard/Dashboard';
+import LoginPage from './LoginPage/LoginPage';
+
+
+function App () {
+  useEffect(() => {
+    connectWithWebSocket();
+  }, []);
+
+  return (
+    <Router>
+      <Switch>
+        <Route path='/dashboard' exact>
+          <Dashboard />
+        </Route>
+        <Route path='/' exact>
+          <LoginPage />
+        </Route>
+        
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
